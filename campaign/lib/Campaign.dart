cb85b57e8f893e5bc7eb9f4a38ab263eecd2544c
Kang soyeong <korea020808@naver.com>
2022-03-09 01:00:26 +0900

    add campaigndate

--- campaign/lib/Campaign.dart
+++ campaign/lib/Campaign.dart
@@ -5,13 +5,16 @@ class Campaign {
   String CampaignScrap = '';
   String CampaignImage = '';
   String CampaignInfo = '';
+  String CampaignDate = '';
 
-  Campaign(int campaign_num, String campaigntitle, String like, String scrap, String image, String info){
+
+  Campaign(int campaign_num, String campaigntitle, String like, String scrap, String image, String info, String campaigndate){
     CampaignNum = campaign_num;
     CampaignTitle = campaigntitle;
     CampaignLike = like;
     CampaignScrap = scrap;
     CampaignImage = image;
     CampaignInfo = info;
+    CampaignDate = campaigndate;
   }
 }
